9e1fd03bc719bcf4bcaed2bfc7b275e10b624e69
Aarushnautiyal <sandeepnautiyal1115@gmail.com>
2022-02-06 15:35:43 +0530

    make ready

--- src/components/Main/Card.jsx
+++ src/components/Main/Card.jsx
@@ -1,11 +1,11 @@
 import React, { useEffect, useState } from 'react'
 
-const Card = (props) => {
+const Card = ({name, subBreedhandler}) => {
     const [dogImage, setDogImage] = useState('')
     
     // fetching the data once as the breed props comes in
     useEffect(() => {
-        fetch(`https://dog.ceo/api/breed/${props.name}/images/random`)
+        fetch(`https://dog.ceo/api/breed/${name}/images/random`)
         .then(res=>res.json())
         .then(data=>setDogImage(data))
     }, [])
@@ -13,7 +13,7 @@ const Card = (props) => {
     return (
         <div className='card'>
             {dogImage&& <img src={dogImage.message} alt=""/>}
-            <h3 className='click--navigate'onClick={props.subBreedhandler}>{props.name}</h3>
+            <h3 className='click--navigate'onClick={subBreedhandler}>{name}</h3>
         </div>
     )
 }
